7bcc9051079c100b26528b39a43be4f88e4f17dc
MrChami119 <154076315+MrChami119@users.noreply.github.com>
2025-05-07 11:15:48 +0530

    Add files via upload

---
+++ SecEx/src/main/java/com/example/SecEx/SecExApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecExApplication.class, args);
+	}
+
+}
