bee5d28c0e7e31796265f50ac4b17a2d90448ff9
MrChami119 <mrchamika119@gmail.com>
2025-05-09 00:30:52 +0530

    Update UserController.java

--- SecEx/src/main/java/com/example/SecEx/controller/UserController.java
+++ SecEx/src/main/java/com/example/SecEx/controller/UserController.java
@@ -14,7 +14,12 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/register")
-    public Users register(@RequestBody Users user){
+    public Users register(@RequestBody Users user) {
         return service.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Users user) {
+        return service.verify(user);
     }
 }
